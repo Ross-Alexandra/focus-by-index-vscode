fb973efe5450b89679e39f6cd8e21f6447e5643f
Ross Alexandra <ross-alexandra@outlook.com>
2026-02-18 20:16:36 -0800

    Fix bug causing overlaping filename to always map to the first file

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,120 @@
 import * as vscode from 'vscode';
 
-function convertTabsToQuickItem(tabs: vscode.Tab[]): vscode.QuickPickItem[] {
-	return structuredClone(
-		tabs.map((tab, index) => ({
-			label: `${index + 1}: ${tab.label}`,
-		})),
+type TabInputTextTab = vscode.Tab & { input: vscode.TabInputText };
+
+interface ComputedPathChunk {
+	overlap: number;
+	fullPaths: string[];
+}
+
+/**
+ * Tokens that separate the index from the filename.
+ */
+const INDEX_SEPARATOR = ': ';
+
+/**
+ * Computes the path depth for the given path.
+ *
+ * @param path the path to find the depth of
+ * @returns the depth of the path measured in number of directories the file is in + 1
+ * @example getPathDepth('I/am/a/file.ts') => 4;
+ */
+function getPathDepth(path: string): number {
+	return path.split('/').length;
+}
+
+/**
+ * Find the maximum overlap value present in the computedChunks
+ *
+ * @param computedChunks The computed chunks to find the max overlap of
+ * @returns The max overlap value in the computed chunks
+ */
+function getMaxOverlap(computedChunks: Record<string, ComputedPathChunk>): number {
+	return Math.max(...Object.values(computedChunks).map(chunk => chunk.overlap));
+}
+
+/**
+ * Compute an initial set of computed path chunks. These chunks have no guarantee
+ * of an overlap = 1.
+ * 
+ * @param paths An array of strings containing the paths to compute initial overlaps
+ * @returns The initial {@link ComputedPathChunk} for these paths.
+ */
+function computeInitialPathOverlaps(paths: string[]): Record<string, ComputedPathChunk> {
+	return paths.reduce<Record<string, ComputedPathChunk>>((computedPathChunks, path) => {
+		const minimizedPath = path.split('/').at(-1);
+		if (!minimizedPath) {
+			return computedPathChunks;
+		}
+
+		if (minimizedPath in computedPathChunks) {
+			computedPathChunks[minimizedPath].overlap += 1;
+			computedPathChunks[minimizedPath].fullPaths.push(path);
+		} else {
+			computedPathChunks[minimizedPath] = {
+				fullPaths: [path],
+				overlap: 1,
+			};
+		}
+
+		return computedPathChunks;
+	}, {});
+}
+
+/**
+ * Increases path depth of the path keys for the {@link computedChunks} argument for {@link ComputedPathChunk}s which
+ * have an {@link ComputedPathChunk.overlap} greater than 1.
+ *
+ * @param computedChunks A {@link ComputedPathChunk} which may or may not have maxOverlap === 1
+ * @returns A {@link ComputedPathChunk} which has maxOverlap which is one less than the {@link computedChunks} argument
+ */
+function increasePathDepthToReduceDeepOverlaps(computedChunks: Record<string, ComputedPathChunk>): Record<string, ComputedPathChunk> {
+	return Object.entries(computedChunks).reduce<Record<string, ComputedPathChunk>>((nextPathChunks, [key, chunk]) => {
+		if (chunk.overlap === 1) {
+			nextPathChunks[key] = chunk;
+			return nextPathChunks;
+		}
+
+		// The next key is the 
+		for (const fullPath of chunk.fullPaths) {
+			const pathParts = fullPath.split('/');
+			const nextKey = pathParts.slice(-1 * getPathDepth(key) - 1, pathParts.length).join('/');
+
+			if (nextKey in nextPathChunks) {
+				nextPathChunks[nextKey].overlap += 1;
+				nextPathChunks[nextKey].fullPaths.push(fullPath);
+			} else {
+				nextPathChunks[nextKey] = {
+					overlap: 1,
+					fullPaths: [fullPath]
+				};
+			}
+		}
+		return nextPathChunks;
+	}, {});
+}
+
+function convertTabsToQuickItem(tabs: TabInputTextTab[]): vscode.QuickPickItem[] {
+	const tabFullPaths = tabs.map(tab => tab.input.uri.path);
+	let tabPathChunks = computeInitialPathOverlaps(tabFullPaths);
+
+	// If any of the overlaps is not 1 then we need to attempt to walk up the tree
+	// for that path.
+	// Do this to make the operation O(DirectoryDepth * N) instead of O(N^2).
+	while (getMaxOverlap(tabPathChunks) > 1) {
+		tabPathChunks = increasePathDepthToReduceDeepOverlaps(tabPathChunks);
+	}
+
+	// Invert the map so that full Paths map to their minified paths computed to
+	// have no overlap.
+	const invertedChunks = Object.fromEntries(
+		Object.entries(tabPathChunks)
+			.flatMap(([key, chunk]) => chunk.fullPaths.map(fullPath => [fullPath, key]))
 	);
+
+	return tabs.map((tab, index) => ({
+		label: `${index + 1}${INDEX_SEPARATOR}${invertedChunks[tab.input.uri.path]}`,
+	}));
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -15,7 +124,9 @@ export function activate(context: vscode.ExtensionContext) {
 		quickPick.placeholder = 'Pick editor by index';
 		const tabGroups = vscode.window.tabGroups.all;
 
-		const allItems = tabGroups.flatMap(tabGroup => tabGroup.tabs);
+		const allItems = tabGroups
+			.flatMap(tabGroup => tabGroup.tabs)
+			.filter(tab => tab.input instanceof vscode.TabInputText) as TabInputTextTab[];
 
 		quickPick.onDidChangeValue(value => {
 			if (value.length === 1 && allItems.length < 10 && /\d+/.test(value)) {
@@ -23,7 +134,7 @@ export function activate(context: vscode.ExtensionContext) {
 				const selectedItem = quickPick.items.at(index - 1);
 				if (!selectedItem) {
 					return;
-				}				
+				}
 
 				quickPick.activeItems = [selectedItem];
 				handleAccept();
@@ -41,9 +152,14 @@ export function activate(context: vscode.ExtensionContext) {
 				return;
 			}
 
-			const selectedTab = allItems.filter(tab => selection.label.includes(tab.label)).at(0);
+			const selectionLabelText = selection.label.split(INDEX_SEPARATOR).at(-1);
+			if (!selectionLabelText) {
+				return;
+			}
 
-			if (!selectedTab || !(selectedTab.input instanceof vscode.TabInputText)) {
+			const selectedTab = allItems.filter(tab => tab.input.uri.path.includes(selectionLabelText)).at(0);
+
+			if (!selectedTab) {
 				vscode.window.showErrorMessage('An error occurred selecting the tab. Please try again later.');
 				return;
 			}
@@ -62,4 +178,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
